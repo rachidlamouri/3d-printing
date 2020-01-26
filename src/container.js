4d6d4ab6f21527b9f840b0ffe76cb0a0788e4d45
Rachid Lamouri <rachidlamouri@gmail.com>
2020-01-26 17:39:50 -0500

    Update makeContainer to enforce side length multiple

--- src/container.js
+++ src/container.js
@@ -1,3 +1,16 @@
+const { logger } = require('./logger');
 const { makeContainer } = require('./makeContainer.js');
 
-global.main = () => makeContainer();
+global.main = () => {
+  const {
+    container,
+    ...meta
+  } = makeContainer({
+    innerWidth: undefined,
+    innerDepth: undefined,
+    outerHeight: undefined,
+  });
+
+  logger.log(meta);
+  return container;
+};
--- src/makeContainer.js
+++ src/makeContainer.js
@@ -1,27 +1,63 @@
-const bottomThickness = 1;
+const _ = require('lodash');
+
+const getNextMultiple = (value, multiplier) => Math.ceil(value / multiplier) * multiplier;
 
 module.exports.makeContainer = ({
   innerWidth = 20,
   innerDepth = 20,
   outerHeight = 20,
+  sideLengthMultiple = 2,
+  minWallThickness = 2,
+  bottomThickness = 1,
 } = {}) => {
-  const wallThickness = 2;
+  [
+    [innerWidth, 'innerWidth'],
+    [innerDepth, 'innerDepth'],
+    [outerHeight, 'outerHeight'],
+    [sideLengthMultiple, 'sideLengthMultiple'],
+  ].forEach(([value, valueName]) => {
+    if (!_.isInteger(value)) {
+      throw new Error(`${valueName} must be an integer`);
+    }
+  });
+
+  const innerHeight = outerHeight - bottomThickness;
+
+  const minOuterWidth = innerWidth + 2 * minWallThickness;
+  const minOuterDepth = innerDepth + 2 * minWallThickness;
+  const outerWidth = getNextMultiple(minOuterWidth, sideLengthMultiple);
+  const outerDepth = getNextMultiple(minOuterDepth, sideLengthMultiple);
+
+  const widthWallThickness = (outerWidth - innerWidth) / 2;
+  const depthWallThickness = (outerDepth - innerDepth) / 2;
 
   const outerBoxSize = [
-    innerWidth + 2 * wallThickness,
-    innerDepth + 2 * wallThickness,
+    outerWidth,
+    outerDepth,
     outerHeight,
   ];
 
   const innerBoxSize = [
     innerWidth,
     innerDepth,
-    outerHeight - bottomThickness,
+    innerHeight,
   ];
 
   const outerBox = cube({ size: outerBoxSize });
   const innerBox = cube({ size: innerBoxSize })
-    .translate([wallThickness, wallThickness, bottomThickness]);
+    .translate([widthWallThickness, depthWallThickness, bottomThickness]);
+  const container = difference(outerBox, innerBox);
 
-  return difference(outerBox, innerBox);
+  const containerMeta = {
+    container,
+    innerBoxSize,
+    outerBoxSize,
+    wallThicknessSize: [
+      widthWallThickness,
+      depthWallThickness,
+      bottomThickness,
+    ],
+  };
+
+  return containerMeta;
 };

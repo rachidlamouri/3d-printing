2cc056487e1cd138a491483eb711f5ac66a67e10
Rachid Lamouri <rachidlamouri@gmail.com>
2020-02-22 12:38:16 -0500

    Add makeDamageContainer

--- src/journeysInMiddleEarth.js
+++ src/journeysInMiddleEarth.js
@@ -1,10 +1,12 @@
 const { makeCardRail } = require('./makeCardRail');
+const { makeDamageContainer } = require('./makeDamageContainer');
 
 global.main = () => {
   const meta = {
     cardRail: makeCardRail(4),
+    damageContainer: makeDamageContainer(3),
   };
 
-  const { entity } = meta.cardRail;
+  const { entity } = meta.damageContainer;
   return entity;
 };
---
+++ src/makeDamageContainer.js
@@ -0,0 +1,113 @@
+const _ = require('lodash');
+const { makeContainer } = require('./makeContainer');
+
+const { miniCard } = require('./lotrConstants');
+const {
+  sizeToMeta,
+  assembleMeta,
+} = require('./lib/utils');
+
+const createBlock = ({
+  stepHeight,
+  baseHeight,
+  numberOfCards,
+  widthOffset,
+}) => {
+  const size = [
+    miniCard.width + (widthOffset * (numberOfCards - 1)),
+    miniCard.depth,
+    baseHeight + numberOfCards * stepHeight,
+  ];
+
+  return {
+    block: {
+      ...sizeToMeta(size),
+      cube: cube({ size }),
+    },
+  };
+};
+
+const createCards = ({
+  baseHeight,
+  numberOfCards,
+  block,
+  stepHeight,
+  widthOffset,
+}) => {
+  const cardHoles = _.range(numberOfCards).map((index) => {
+    const size = [
+      miniCard.width,
+      miniCard.depth,
+      block.height,
+    ];
+
+    return cube({ size })
+      .translate([
+        widthOffset * index,
+        0,
+        baseHeight + index * stepHeight,
+      ])
+      .setColor([0, 0, 1]);
+  });
+
+  return {
+    cardHoles,
+  };
+};
+
+const createOuterContainer = ({
+  block,
+  stepHeight,
+}) => {
+  const wallThickness = 0.8;
+
+  const outerContainer = makeContainer({
+    innerWidth: block.width,
+    innerDepth: block.depth,
+    outerHeight: Math.ceil(block.height + stepHeight),
+    ignoreDecimalPrecision: true,
+    bottomClearance: 0,
+    wallThickness,
+  });
+
+  outerContainer.container = outerContainer.container.translate([-wallThickness, -wallThickness, 0]);
+
+  return {
+    outerContainer,
+  };
+};
+
+const createEntity = ({
+  block,
+  cardHoles,
+  outerContainer,
+}) => {
+  const entity = union(
+    difference(
+      block.cube,
+      ...cardHoles,
+    ),
+    outerContainer.container,
+  );
+
+  return {
+    entity,
+  };
+};
+
+module.exports.makeDamageContainer = (numberOfCards) => {
+  const initialParameters = {
+    baseHeight: 0.6,
+    stepHeight: 1,
+    widthOffset: 16,
+    numberOfCards,
+  };
+
+  return assembleMeta(
+    initialParameters,
+    createBlock,
+    createCards,
+    createOuterContainer,
+    createEntity,
+  );
+};

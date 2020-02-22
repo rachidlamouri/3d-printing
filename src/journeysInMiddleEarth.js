b29396f34e9caa72428d4bbc1d9f059ae00df868
Rachid Lamouri <rachidlamouri@gmail.com>
2020-02-22 13:56:46 -0500

    Add makeCardBank

--- src/journeysInMiddleEarth.js
+++ src/journeysInMiddleEarth.js
@@ -1,10 +1,16 @@
 const { makeCardRail } = require('./makeCardRail');
 const { makeDamageContainer } = require('./makeDamageContainer');
+const { makeCardBank } = require('./makeCardBank');
 
 global.main = () => {
   const meta = {
     cardRail: makeCardRail(4),
     damageContainer: makeDamageContainer(3),
+    drawDiscard: makeCardBank({
+      numberOfCards: 2,
+      outerHeight: 5,
+      dividerThickness: 0.8,
+    }),
   };
 
   const { entity } = meta.damageContainer;
---
+++ src/makeCardBank.js
@@ -0,0 +1,124 @@
+const _ = require('lodash');
+const { makeRepeatContainer } = require('./makeRepeatContainer');
+
+const { miniCard } = require('./lotrConstants');
+const {
+  sizeToMeta,
+  positionToMeta,
+  assembleMeta,
+} = require('./lib/utils');
+
+const createBaseContainer = ({
+  numberOfCards,
+  outerHeight,
+  dividerThickness,
+  bottomThickness,
+  bottomClearance,
+}) => {
+  const baseContainerMeta = makeRepeatContainer({
+    count: numberOfCards,
+    innerWidth: miniCard.width,
+    innerDepth: miniCard.depth,
+    outerHeight,
+    dividerThickness,
+    bottomClearance,
+    bottomThickness,
+  });
+
+  return {
+    baseContainerMeta,
+  };
+};
+
+const createThumbHoles = ({
+  baseContainerMeta,
+  dividerThickness,
+  bottomThickness,
+  thumbHoleWidth,
+  bottomClearance,
+}) => {
+  const wallHoleMetas = baseContainerMeta.repeatedEntities.map(({ xOffset, containerMeta }) => {
+    const size = [
+      containerMeta.innerWidth,
+      dividerThickness,
+      containerMeta.outerHeight,
+    ];
+
+    const position = [
+      dividerThickness + xOffset,
+      0,
+      bottomThickness,
+    ];
+
+    return {
+      ...sizeToMeta(size),
+      ...positionToMeta(position),
+      entity: cube({ size })
+        .translate(position),
+    };
+  });
+
+  const thumbHoleMetas = wallHoleMetas.map((wallHoleMeta) => {
+    const size = [
+      thumbHoleWidth,
+      bottomClearance / 2,
+      bottomThickness,
+    ];
+
+    const position = [
+      wallHoleMeta.x + (wallHoleMeta.width / 2) - (thumbHoleWidth / 2),
+      0,
+      0,
+    ];
+
+    return {
+      ...sizeToMeta(size),
+      ...positionToMeta(position),
+      entity: cube({ size })
+        .translate(position),
+    };
+  });
+
+  return {
+    wallHoleMetas,
+    thumbHoleMetas,
+  };
+};
+
+const createEntity = ({
+  baseContainerMeta,
+  wallHoleMetas,
+  thumbHoleMetas,
+}) => {
+  const entity = difference(
+    baseContainerMeta.container,
+    ..._.map(wallHoleMetas, 'entity'),
+    ..._.map(thumbHoleMetas, 'entity'),
+  );
+
+  return {
+    entity,
+  };
+};
+
+module.exports.makeCardBank = ({
+  numberOfCards = 2,
+  outerHeight,
+  dividerThickness,
+}) => {
+  const initialParameters = {
+    numberOfCards,
+    outerHeight,
+    dividerThickness,
+    bottomClearance: 5,
+    bottomThickness: 0.6,
+    thumbHoleWidth: 20,
+  };
+
+  return assembleMeta(
+    initialParameters,
+    createBaseContainer,
+    createThumbHoles,
+    createEntity,
+  );
+};

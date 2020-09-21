2534e3b104689e4304a8b873fc5a2e2184756a27
Rachid Lamouri <rachidlamouri@gmail.com>
2020-09-20 20:16:16 -0400

    Add kitchen organizers

---
+++ src/organizers/kitchen.main.js
@@ -0,0 +1,46 @@
+const { buildMakeContainerWithDefaults } = require('../lib/makeContainer');
+const { buildMakeRepeatContainerWithDefaults } = require('../lib/makeRepeatContainer');
+
+const defaultWallThickness = 1;
+const defaults = {
+  sideLengthMultiple: 1,
+  bottomThickness: 0.5,
+};
+
+const makeContainer = buildMakeContainerWithDefaults({
+  minWallThickness: defaultWallThickness,
+  ...defaults,
+});
+
+const makeRepeatContainer = buildMakeRepeatContainerWithDefaults({
+  dividerThickness: defaultWallThickness,
+  ...defaults,
+});
+
+global.main = () => {
+  const metaMap = {
+    measuringCups: makeContainer({
+      innerWidth: 160,
+      innerDepth: 90,
+      outerHeight: 40,
+      bottomClearance: 20,
+      baseClearance: 20,
+      xClearance: 20,
+      yClearance: 20,
+    }),
+    chipClips: makeRepeatContainer({
+      count: 6,
+      innerWidth: 36,
+      innerDepth: 85,
+      outerHeight: 20,
+    }),
+  };
+
+  const {
+    debug,
+    entity,
+  } = metaMap.chipClips;
+
+  logger.log(debug);
+  return entity;
+};

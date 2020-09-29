96bfa2c9a1660ef2261d7b6cc85371b1dd9edd19
Rachid Lamouri <rachidlamouri@gmail.com>
2020-09-29 17:25:36 -0400

    Add avocondo to kithen organizers

--- src/organizers/kitchen.map.js
+++ src/organizers/kitchen.map.js
@@ -1,3 +1,7 @@
+const {
+  primitives3d: { cube },
+  booleanOps: { difference, union },
+} = require('@jscad/csg/api');
 const { buildMakeContainerWithDefaults } = require('../lib/makeContainer');
 const { buildMakeRepeatContainerWithDefaults } = require('../lib/makeRepeatContainer');
 
@@ -17,7 +21,77 @@ const makeRepeatContainer = buildMakeRepeatContainerWithDefaults({
   ...defaults,
 });
 
+const avocondoWallThickness = 6;
+
 const map = {
+  avocondo: () => {
+    const avocadoDiameter = 70;
+    const wallThickness = avocondoWallThickness;
+    const arbitrarilyLargeSize = 500;
+
+    const box = difference(
+      cube([
+        avocadoDiameter + 2 * wallThickness,
+        avocadoDiameter,
+        avocadoDiameter + 2 * wallThickness,
+      ]),
+      cube([avocadoDiameter, avocadoDiameter, avocadoDiameter])
+        .translate([wallThickness, 0, wallThickness]),
+    );
+
+    const support = difference(
+      box.rotateY(45),
+      cube(arbitrarilyLargeSize, arbitrarilyLargeSize, arbitrarilyLargeSize),
+    );
+
+    const innerDiagonal = Math.sqrt(2 * (avocadoDiameter ** 2));
+    const innerCornerToOuterCornerDistance = wallThickness / Math.sin(45 * (Math.PI / 180));
+
+    const entity = union(
+      support.translate([0, 0, innerDiagonal + innerCornerToOuterCornerDistance]),
+      support.rotateX(180).translate([0, avocadoDiameter]),
+    );
+
+    return { entity };
+  },
+  avocondoClip: () => {
+    const tolerance = 0.1;
+    const wallThickness = 1;
+    const wallHeight = 5;
+    const baseHeight = 0.2;
+
+    const lowerArmDepth = 2 * wallThickness + avocondoWallThickness + tolerance;
+    const lowerArmWidth = 20;
+    const fullBaseSideLength = lowerArmWidth + lowerArmDepth;
+
+    const base = difference(
+      cube([fullBaseSideLength, fullBaseSideLength, baseHeight]),
+      cube([lowerArmWidth, lowerArmWidth, baseHeight])
+        .translate([lowerArmDepth, lowerArmDepth]),
+    );
+
+    const outerWall = difference(
+      cube([fullBaseSideLength, fullBaseSideLength, wallHeight]),
+      cube([fullBaseSideLength - wallThickness, fullBaseSideLength - wallThickness, wallHeight])
+        .translate([wallThickness, wallThickness]),
+    )
+      .translate([0, 0, baseHeight]);
+
+    const innerWall = difference(
+      cube([lowerArmWidth + wallThickness, lowerArmWidth + wallThickness, wallHeight]),
+      cube([lowerArmWidth, lowerArmWidth, wallHeight])
+        .translate([wallThickness, wallThickness]),
+    )
+      .translate([wallThickness + avocondoWallThickness + tolerance, wallThickness + avocondoWallThickness + tolerance, baseHeight]);
+
+    const entity = union(
+      base,
+      outerWall,
+      innerWall,
+    );
+
+    return { entity };
+  },
   measuringCups: () => makeContainer({
     innerWidth: 160,
     innerDepth: 90,
@@ -48,7 +122,7 @@ const getParameterDefinitions = () => [
 const main = ({ name }) => map[name]().entity;
 
 module.exports = {
-  objectNames: Object.keys(map),
+  objectNames: Object.keys(map).filter((a) => a.startsWith('avo')),
   getParameterDefinitions,
   main,
 };

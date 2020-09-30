26c84929c96cfc0c0269b2efedd1ed9dfcc1203b
Rachid Lamouri <rachidlamouri@gmail.com>
2020-09-30 19:44:18 -0400

    Update makeBoxAndLid to take configurable wall thickness and lid height

--- src/lib/makeBoxAndLid.js
+++ src/lib/makeBoxAndLid.js
@@ -6,15 +6,22 @@ module.exports.makeBoxAndLid = ({
   innerWidth = 20,
   innerDepth = 20,
   innerHeight = 19.2,
-  bottomAndTopThickness = 0.3,
+  bottomAndTopThickness = 0.2,
+  wallThickness = 0.8,
   lidAllowance = 0.1,
+  lidHeightPercentage = 1,
 }) => {
-  const wallThickness = 1;
-
   const parameters = { lidAllowance };
 
   validateParameters(parameters, {}, {
     lidAllowance: () => Joi.number().precision(1).required(),
+    lidHeightPercentage: () => (
+      Joi.number()
+        .precision(1)
+        .min(0.01)
+        .max(1)
+        .required()
+    ),
   });
 
   const boxOuterHeight = innerHeight + bottomAndTopThickness;
@@ -31,7 +38,7 @@ module.exports.makeBoxAndLid = ({
   const lid = makeContainer({
     innerWidth: box.finalDimensions.width + 2 * lidAllowance,
     innerDepth: box.finalDimensions.depth + 2 * lidAllowance,
-    outerHeight: boxOuterHeight + 2 * lidAllowance,
+    outerHeight: (lidHeightPercentage * boxOuterHeight) + 2 * lidAllowance,
     wallThickness,
     bottomThickness: bottomAndTopThickness,
     minBottomHoleSideLength: 0,

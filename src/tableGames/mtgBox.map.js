49fa1a29ec68227199947a13fbcfac3fc43338b3
Rachid Lamouri <rachidlamouri@gmail.com>
2020-10-05 20:52:54 -0400

    Add full box and lid csg to mtgBox

--- src/tableGames/mtgBox.map.js
+++ src/tableGames/mtgBox.map.js
@@ -1,21 +1,22 @@
 const { makeBoxAndLid } = require('../lib/makeBoxAndLid');
 
-const { box, lid } = makeBoxAndLid({
+const { box, lid, sideBySide } = makeBoxAndLid({
   innerWidth: 89,
   innerDepth: 64,
   innerHeight: 40,
-  bottomAndTopThickness: 0.1,
+  bottomAndTopThickness: 0.4,
   wallThickness: 0.8,
-  lidHeightPercentage: 0.1,
+  lidHeightPercentage: 0.4,
 });
 
 const map = {
-  box: () => box,
-  lid: () => lid,
+  box: () => box.csg,
+  lid: () => lid.csg,
+  sideBySide: () => sideBySide,
 };
 
 module.exports = {
   objectNames: Object.keys(map),
   getParameterDefinitions: () => [{ name: 'name', type: 'text' }],
-  main: ({ name }) => map[name]().csg,
+  main: ({ name }) => map[name](),
 };
